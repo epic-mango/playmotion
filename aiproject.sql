04caefe336501e6441e857c3c5828fb135a074b4
epic-mango <drany@live.com>
2021-11-24 21:13:15 -0600

    avance Odalis

---
+++ aiproject.sql
@@ -0,0 +1,194 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 25-11-2021 a las 01:58:28
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `aiproject`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `canciones`
+--
+
+CREATE TABLE `canciones` (
+  `id` int(10) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `artista` varchar(100) NOT NULL,
+  `album` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `emociones`
+--
+
+CREATE TABLE `emociones` (
+  `id` int(10) NOT NULL,
+  `emocion` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `emociones`
+--
+
+INSERT INTO `emociones` (`id`, `emocion`) VALUES
+(1, 'mad'),
+(4, 'critical'),
+(5, 'hateful'),
+(6, 'selfish'),
+(7, 'angry'),
+(8, 'hostile'),
+(9, 'hurt'),
+(10, 'skeptical'),
+(11, 'irritated'),
+(12, 'jealous'),
+(13, 'frustrated'),
+(14, 'sarcastic'),
+(15, 'distant'),
+(16, 'scared'),
+(17, 'confused'),
+(18, 'rejected'),
+(19, 'helpless'),
+(20, 'submissive'),
+(21, 'insecure'),
+(22, 'anxious'),
+(23, 'bewildered'),
+(24, 'discouraged'),
+(25, 'insignificant'),
+(26, 'inadequate'),
+(27, 'embarrased'),
+(28, 'overwhelmed'),
+(29, 'joyful'),
+(30, 'excited'),
+(31, 'sensuous'),
+(32, 'energetic'),
+(33, 'selfish'),
+(34, 'cheerful'),
+(35, 'creative'),
+(36, 'hopeful'),
+(37, 'daring'),
+(38, 'fascinating'),
+(39, 'stimulating'),
+(40, 'amused'),
+(41, 'playful'),
+(42, 'optimistic'),
+(43, 'powerful'),
+(44, 'aware'),
+(45, 'proud'),
+(46, 'respect'),
+(47, 'appreciated'),
+(48, 'important'),
+(49, 'faithful'),
+(50, 'confident'),
+(51, 'discerning'),
+(52, 'valueable'),
+(53, 'worthwhile'),
+(54, 'succesful'),
+(55, 'surprised'),
+(56, 'peaceful'),
+(57, 'content'),
+(58, 'thoughtful'),
+(59, 'intimate'),
+(60, 'loving'),
+(61, 'trusting'),
+(62, 'nurturing'),
+(63, 'thankful'),
+(64, 'secure'),
+(65, 'serene'),
+(66, 'responsive'),
+(67, 'pensive'),
+(68, 'relaxed'),
+(69, 'sad'),
+(70, 'guilty'),
+(71, 'ashamed'),
+(72, 'depressed'),
+(73, 'lonely'),
+(74, 'bored'),
+(75, 'tired'),
+(76, 'sleepy'),
+(77, 'apathetic'),
+(78, 'isolated'),
+(79, 'inferior'),
+(80, 'stupid'),
+(81, 'remorseful');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `relaciones`
+--
+
+CREATE TABLE `relaciones` (
+  `idusuario` int(11) NOT NULL,
+  `idcancion` int(11) NOT NULL,
+  `idemocion` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(10) NOT NULL,
+  `usuario` varchar(100) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `contrasenia` varchar(100) NOT NULL,
+  `fechanacimiento` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `canciones`
+--
+ALTER TABLE `canciones`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `emociones`
+--
+ALTER TABLE `emociones`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `emociones`
+--
+ALTER TABLE `emociones`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=82;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ login.php
@@ -0,0 +1,52 @@
+<?php
+//pegar aqui el codigo que mando el profe 
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Authorization, Access-Control-Allow-Methods, Access-Control-Allow-Headers, Allow, Access-Control-Allow-Origin");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS, HEAD");
+header("Allow: GET, POST, PUT, DELETE, OPTIONS, HEAD");
+require_once "conexion.php";
+require_once "jwt.php";
+
+
+if($_SERVER["REQUEST_METHOD"]=="OPTIONS"){
+        exit();
+}
+
+       $h = apache_request_headers(); 
+       //bloque de seguridad
+       $jwt = $h['Authorization'];
+       if(JWT::verify($jwt, Config::FIR)!=0){
+       header("HTTP/1.1 401 Unauthorized");
+        exit();
+       }
+       $datos=JWT::get_data($jwt,Config::FIR);
+    
+//aqui se acaba le bloque de seguridad
+
+$metodo = $_SERVER['REQUEST_METHOD'];
+switch($metodo){
+    case 'GET':
+        if(isset($_GET['usuarios']) && isset($_GET['contrasenia'])){
+            $c = conexion();
+            $s = $c->prepare("SELECT * FROM usuarios WHERE usuario=:u AND contrasenia=sha1(:p)");
+            $s->bindValue(":u", $_GET['usuario']);
+            $s->bindValue(":p", $_GET['contrasenia']);
+            $s->execute();
+            $s->setFetchMode(PDO::FETCH_ASSOC);
+            $r = $s->fetchAll();
+            if($r){
+                $jwt = JWT::create(['usuario'=>$_GET['usuario']], Config::FIR,7200);
+                $m = array('login'=>'yes', 'jwt'=>$jwt);
+            }else{
+                $m = ['login'=>'no'];
+            }
+            header("HTTP/1.1 200 OK");
+            echo json_encode($m);
+        }else{
+            header("HTTP/1.1 400 Bad Request");
+        }
+        break;
+    default:
+        header("HTTP/1.1 400 Bad Request");
+}
